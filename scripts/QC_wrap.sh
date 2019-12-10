641688a917e2f13db786efe3183f7835fb2ab940
abilashdurairaj <33624326+abilashdurairaj@users.noreply.github.com>
2019-12-10 19:00:03 +0100

    Add files via upload

---
+++ scripts/QC_wrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+#$ -l arch=linux-x64
+#$ -b n
+#$ -q all.q
+#$ -i /dev/null
+#$ -e /path/to/log/error/
+#$ -o /path/to/log/output/
+#$ -cwd
+#$ -l vf=3G
+#$ -t 1-4
+
+##### export latest GCC version to the LD_LIBRARY_PATH variable and jave run environment to JAVA_HOME variable
+export JAVA_HOME=/path/to/java/jre1.8.0_191/bin/
+export LD_LIBRARY_PATH=/path/to/gcc_latest/gcc-xx.xx.xx/libstdc++-v3/:${LD_LIBRARY_PATH}
+
+### hh: path where the fastq.gz files are located
+### STRING2: variable storing the sampleIDs
+hh=/path/to/fastq_files/
+hm_dr=/path/to/store/data/
+mapfile -t STRING2 < <(ls ${hh}*.fastq.gz ' | sed 's|.*/||g' | sed 's/_.*//g') 
+
+
+bash /path/to/bash_files/QC_File_job.sh ${STRING2[$SGE_TASK_ID-1]} ${hm_dr} ${hh}
