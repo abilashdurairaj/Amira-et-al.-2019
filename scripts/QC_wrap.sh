b7a87c3131968807857ab06801ee810a69f9b658
abilashdurairaj <33624326+abilashdurairaj@users.noreply.github.com>
2020-03-26 11:59:18 +0100

    Update QC_wrap.sh

--- scripts/QC_wrap.sh
+++ scripts/QC_wrap.sh
@@ -13,10 +13,13 @@
 export JAVA_HOME=/path/to/java/jre1.8.0_191/bin/
 export LD_LIBRARY_PATH=/path/to/gcc_latest/gcc-xx.xx.xx/libstdc++-v3/:${LD_LIBRARY_PATH}
 
-### hh: path where the fastq.gz files are located
-### STRING2: variable storing the sampleIDs
+
+### hh    : path where the fastq.gz files are located
+### hm_dr : path where to store output files 
 hh=/path/to/fastq_files/
 hm_dr=/path/to/store/data/
+
+### STRING2: variable storing the sampleIDs
 mapfile -t STRING2 < <(ls ${hh}*.fastq.gz ' | sed 's|.*/||g' | sed 's/_.*//g') 
 
 
