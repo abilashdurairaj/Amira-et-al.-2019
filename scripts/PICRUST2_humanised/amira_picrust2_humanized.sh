11dd9e0c96d8322d05a0b4ed74c62bc39121d6bc
abilashdurairaj <33624326+abilashdurairaj@users.noreply.github.com>
2020-03-30 12:14:57 +0200

    Update amira_picrust2_humanized.sh

--- scripts/PICRUST2_humanised/amira_picrust2_humanized.sh
+++ scripts/PICRUST2_humanised/amira_picrust2_humanized.sh
@@ -2,9 +2,9 @@
 
 
 conda activate picrust2
-mkdir -p /project/genomics/Abilash/Amira/humanized_PICRUSt2/picrust2_res/
-cd /project/genomics/Abilash/Amira/humanized_PICRUSt2/
+mkdir -p /path/to/humanized_PICRUSt2/picrust2_res/
+cd /path/to/humanized_PICRUSt2/picrust2_res/
 
-picrust2_pipeline.py -s OTUs-Seqs.fasta -i OTUs_Table-norm-picrust.txt  -o /project/genomics/Abilash/Amira/humanized_PICRUSt2/picrust2_res/picrust2_out_pipeline_edited  -p 1
+picrust2_pipeline.py -s OTUs-Seqs.fasta -i OTUs_Table-norm-picrust.txt  -o /path/to/humanized_PICRUSt2/picrust2_res/picrust2_out_pipeline_edited  -p 1
 
 
